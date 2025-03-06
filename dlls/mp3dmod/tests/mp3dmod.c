0b4e29b5b99e0747b2acd485cc5cf99df4a5fc82
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-03-06 19:15:17 +0000

    10.2.r281.g0530ef6faca

--- dlls/mp3dmod/tests/mp3dmod.c
+++ dlls/mp3dmod/tests/mp3dmod.c
@@ -746,6 +746,45 @@ static void test_media_types(void)
     check_dmo_media_type(&mt, &output_mt);
     MoFreeMediaType(&mt);
 
+    output_format.nSamplesPerSec = 24000;
+    output_format.nAvgBytesPerSec = 24000;
+    hr = IMediaObject_SetOutputType(dmo, 0, &output_mt, DMO_SET_TYPEF_TEST_ONLY);
+    ok(hr == S_OK, "Got hr %#lx.\n", hr);
+
+    output_format.nSamplesPerSec = 12000;
+    output_format.nAvgBytesPerSec = 12000;
+    hr = IMediaObject_SetOutputType(dmo, 0, &output_mt, DMO_SET_TYPEF_TEST_ONLY);
+    ok(hr == S_OK, "Got hr %#lx.\n", hr);
+
+    output_format.nSamplesPerSec = 6000;
+    output_format.nAvgBytesPerSec = 6000;
+    hr = IMediaObject_SetOutputType(dmo, 0, &output_mt, DMO_SET_TYPEF_TEST_ONLY);
+    ok(hr == DMO_E_TYPE_NOT_ACCEPTED, "Got hr %#lx.\n", hr);
+
+    output_format.nSamplesPerSec = 12000;
+    output_format.nAvgBytesPerSec = 6000;
+    hr = IMediaObject_SetOutputType(dmo, 0, &output_mt, DMO_SET_TYPEF_TEST_ONLY);
+    ok(hr == E_INVALIDARG, "Got hr %#lx.\n", hr);
+
+    output_format.nSamplesPerSec = 48000;
+    output_format.nAvgBytesPerSec = 48000;
+    output_format.nChannels = 2;
+    hr = IMediaObject_SetOutputType(dmo, 0, &output_mt, DMO_SET_TYPEF_TEST_ONLY);
+    ok(hr == E_INVALIDARG, "Got hr %#lx.\n", hr);
+
+    /* Windows accepts 32 bits per sample but does not enumerate it */
+    output_format.nChannels = 1;
+    output_format.wBitsPerSample = 32;
+    output_format.nBlockAlign = 4;
+    output_format.nSamplesPerSec = 48000;
+    output_format.nAvgBytesPerSec = 192000;
+    hr = IMediaObject_SetOutputType(dmo, 0, &output_mt, DMO_SET_TYPEF_TEST_ONLY);
+    ok(hr == S_OK, "Got hr %#lx.\n", hr);
+
+    mp3fmt.wfx.nChannels = 0;
+    hr = IMediaObject_SetInputType(dmo, 0, &input_mt, DMO_SET_TYPEF_TEST_ONLY);
+    ok(hr == DMO_E_TYPE_NOT_ACCEPTED, "Got hr %#lx.\n", hr);
+
     IMediaObject_Release(dmo);
 }
 
