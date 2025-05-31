b10a97801bf00e18a47b16491bc7296a1f9f21c1
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-05-31 09:13:56 +0100

    10.8.r247.ge0bb1dba3d6

--- batocera/01-git.sh
+++ batocera/01-git.sh
@@ -1,12 +1,13 @@
-find /home/batocera/github/wine-tkg-batocera ! -type d -name '.git' -type f -exec rm -rf {} +
-cd /home/batocera/github/wine-tkg-batocera && find -maxdepth 1 -type d ! -name '.git' -exec rm -rf {} \;
-cd /home/batocera/github/wine-tkg-batocera && find -maxdepth 1 -type f ! -name '.gitattributes' -exec rm -rf {} \;
-mkdir -p /home/batocera/github/wine-tkg-batocera/custom
+find /home/lee/github/wine-tkg-batocera ! -type d -name '.git' -type f -exec rm -rf {} +
+cd /home/lee/github/wine-tkg-batocera && find -maxdepth 1 -type d ! -name '.git' -exec rm -rf {} \;
+cd /home/lee/github/wine-tkg-batocera && find -maxdepth 1 -type f ! -name '.gitattributes' -exec rm -rf {} \;
+mkdir -p /home/lee/github/wine-tkg-batocera/custom
 find /tmp/wine-tkg/src/wine-git -name '.git*' -type f -exec rm -rf {} +
-cd /home/batocera/github/wine-tkg-git/wine-tkg-git
-cp -a /tmp/wine-tkg/src/wine-git/* /home/batocera/github/wine-tkg-batocera
-cp -f customization.cfg last_build_config.log prepare.log /home/batocera/github/wine-tkg-batocera/custom
-rm -rf /home/batocera/github/wine-tkg-batocera/autom4te.cache
-rm -rf /home/batocera/github/wine-tkg-batocera/configure~
-find /home/batocera/github/wine-tkg-batocera -type f -name "*.orig" -exec rm -v {} \;
-find /home/batocera/github/wine-tkg-batocera -type f -name "*.rej" -exec rm -v {} \;
+cd /home/lee/github/wine-tkg-git/wine-tkg-git
+cp -a /tmp/wine-tkg/src/wine-git/* /home/lee/github/wine-tkg-batocera
+cp -f customization.cfg last_build_config.log prepare.log /home/lee/github/wine-tkg-batocera/custom
+cp -a /home/lee/github/batocera.tkg-wine /home/lee/github/wine-tkg-batocera/batocera
+rm -rf /home/lee/github/wine-tkg-batocera/autom4te.cache
+rm -rf /home/lee/github/wine-tkg-batocera/configure~
+find /home/lee/github/wine-tkg-batocera -type f -name "*.orig" -exec rm -v {} \;
+find /home/lee/github/wine-tkg-batocera -type f -name "*.rej" -exec rm -v {} \;
