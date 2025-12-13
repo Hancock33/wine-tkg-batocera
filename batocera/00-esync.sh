dbf30e13e03d91b894262fd1a459c49bd3ccd76e
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-12-13 09:07:04 +0000

    ntsynch is part of the upstream master

--- batocera/00-esync.sh
+++
@@ -1,3 +0,0 @@
-cd ~/github/wine-tkg-git/wine-tkg-git
-cp ./batocera/customization.cfg.esync customization.cfg
-./non-makepkg-build.sh
--- batocera/00-ntsync.sh
+++
@@ -1,3 +0,0 @@
-cd ~/github/wine-tkg-git/wine-tkg-git
-cp ./batocera/customization.cfg.ntsync customization.cfg
-./non-makepkg-build.sh
